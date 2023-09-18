252bab97b5bac848977c9c9cf62413114eb7ec4b
journeyman32 <bryan@coderbox.co>
2023-09-18 00:20:11 -0500

    exports error and output to text files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use clap::{App,Arg};
+use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("echor")
         .version("0.1.0")
         .author("Bryan Pritchett <bryan@coderbox.co")
         .about("Rust Echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Don't print newline")
+                .takes_value(false),
+        )
         .get_matches();
 
-    //println!("{:?}",matches);
+    println!("{:#?}", matches);
 }
